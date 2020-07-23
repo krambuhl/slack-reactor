70747443e512dc921c0650f42f3e63a92d4b8dc7
Evan Krambuhl <evan.krambuhl@gmail.com>
2020-07-23 16:07:53 -0700

    event test

---
+++ server.js
@@ -0,0 +1,14 @@
+const { createEventAdapter } = require('@slack/events-api');
+const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
+const slackEvents = createEventAdapter(slackSigningSecret);
+
+// Read the port from the environment variables, fallback to 3000 default.
+const port = process.env.PORT || 3000;
+
+(async () => {
+  // Start the built-in server
+  const server = await slackEvents.start(port);
+
+  // Log a message when the server is ready
+  console.log(`Listening for events on ${server.address().port}`);
+})();

8f95e33543b5a6474addb5465c6a7428ff899096
Evan Krambuhl <evan.krambuhl@gmail.com>
2020-07-23 17:01:16 -0700

    error handling

--- api/event.js
+++ api/event.js
@@ -1,9 +1,23 @@
 const VERIFICATION_TOKEN = process.env.SLACK_VERIFICATION_TOKEN
 
 module.exports = (req, res) => {
-  const { token, ...body } = req.body
+  try {
+    const { token, ...body } = req.body
 
-  if (token === VERIFICATION_TOKEN) {
-    console.log(body)
+    if (token === VERIFICATION_TOKEN) {
+      console.log(body)
+
+      res
+        .status(200)
+        .json({ body })
+    } else {
+      res
+        .status(401)
+        .json({ error: 'Unauthorized' })
+    }
+  } catch (err) {
+    res
+      .status(400)
+      .json({ error: 'Bad Request' })
   }
 }

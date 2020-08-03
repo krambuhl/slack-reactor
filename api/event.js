f783375393492a357c07700b1b2e4f9d7cceee25
Evan Krambuhl <evan.krambuhl@gmail.com>
2020-08-03 15:54:24 -0700

    try pusher

--- api/event.js
+++ api/event.js
@@ -1,23 +1,51 @@
-const VERIFICATION_TOKEN = process.env.SLACK_VERIFICATION_TOKEN
+const Pusher = require('pusher');
+
+const {
+  VERIFICATION_TOKEN,
+  PUSHER_APP_ID,
+  PUSHER_KEY,
+  PUSHER_SECRET
+} = process.env
+
+const pusher = new Pusher({
+  appId: PUSHER_APP_ID,
+  key: PUSHER_KEY,
+  secret: PUSHER_SECRET,
+  cluster: 'us3',
+  encrypted: true
+})
+
+function sendEvent(event) {
+  const { type, user, item_user, reaction } = event
+
+  pusher.trigger('emoji-reaction', type, {
+    user,
+    item_user,
+    reaction
+  });
+}
 
 module.exports = (req, res) => {
   try {
-    const { token, ...body } = req.body
+    const { token } = req.body
 
     if (token === VERIFICATION_TOKEN) {
-      console.log(body)
+      const { event, type, challenge } = req.body
 
-      res
-        .status(200)
-        .json({ body })
+      // verify to slack that we are the real deal
+      if (type === 'url_verification') {
+        res.status(200).json({ challenge })
+      }
+
+      // handle event
+      if (type === 'event_callback') {
+        sendEvent(event)
+        res.status(200).json({ success: true })
+      }
     } else {
-      res
-        .status(401)
-        .json({ error: 'Unauthorized' })
+      res.status(401).json({ success: false, error: 'Unauthorized' })
     }
   } catch (err) {
-    res
-      .status(400)
-      .json({ error: 'Bad Request' })
+    res.status(400).json({ success: false, error: 'Bad Request' })
   }
 }

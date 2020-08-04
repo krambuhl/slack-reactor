321588769a36254fa26795e4a743dcfc5ddc57c3
Evan Krambuhl <evan.krambuhl@gmail.com>
2020-08-03 17:11:36 -0700

    handle commands and fallback

--- api/event.js
+++ api/event.js
@@ -1,3 +1,4 @@
+const sendCommand = require('../lib/sendCommand')
 const sendEvent = require('../lib/sendEvent')
 
 const { SLACK_VERIFICATION_TOKEN } = process.env
@@ -7,20 +8,35 @@ module.exports = (req, res) => {
     const { token } = req.body
 
     if (token === SLACK_VERIFICATION_TOKEN) {
-      const { event, type, challenge, ...rest } = req.body
+      const { type, command } = req.body
 
       console.log(req.body)
 
       // verify to slack that we are the real deal
       if (type === 'url_verification') {
+        const { challenge } = req.body
         res.status(200).json({ challenge })
       }
 
-      // handle event
-      if (type === 'event_callback') {
+      // send events
+      else if (type === 'event_callback') {
+        const { event } = req.body
         sendEvent(event)
           .then(() => res.status(200).json({ success: true }))
           .catch(() => res.status(500).json({ success: false }))
+      }
+
+      // send commands
+      else if (command !== undefined) {
+        sendCommand(req.body)
+          .then(() => res.status(200).json({ success: true }))
+          .catch(() => res.status(500).json({ success: false }))
+      }
+
+      // catch everything else
+      else {
+        console.log({ success: false, error: 'Unhandled Event' })
+        res.status(400).json({ success: false, error: 'Unhandled Event' })
       }
     } else {
       console.log({ success: false, error: 'Unauthorized' })
